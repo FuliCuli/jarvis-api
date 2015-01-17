caad9698b5b293e833ecde383d221ee88eb3a953
xherriot <xherriot@gmail.com>
2015-01-17 11:34:02 +0000

    feat(api): add location type

---
+++ api/controllers/LocationTypeController.js
@@ -0,0 +1,11 @@
+/**
+ * LocationTypeController
+ *
+ * @description :: Server-side logic for managing Locationtypes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
