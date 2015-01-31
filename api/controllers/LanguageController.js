188e30970aec0856cde1102cde6154cfefb91ddf
xherriot <xherriot@gmail.com>
2015-01-31 15:24:27 +0000

    feat(*): add language, object & service api's

---
+++ api/controllers/LanguageController.js
@@ -0,0 +1,11 @@
+/**
+ * LanguageController
+ *
+ * @description :: Server-side logic for managing Languages
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/ObjectController.js
@@ -0,0 +1,11 @@
+/**
+ * ObjectController
+ *
+ * @description :: Server-side logic for managing Objects
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
