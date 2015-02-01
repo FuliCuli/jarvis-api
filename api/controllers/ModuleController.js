5d7257d69f839f034a05ed6252edeb96224d8441
xherriot <xherriot@gmail.com>
2015-02-01 10:02:31 +0000

    fix(*): change Object to Module name

--- api/controllers/ModuleController.js
+++ api/controllers/ModuleController.js
@@ -1,7 +1,7 @@
 /**
- * ObjectController
+ * ModuleController
  *
- * @description :: Server-side logic for managing Objects
+ * @description :: Server-side logic for managing Modules
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
